2d2beee0f771158800f99506c951137478286e94
pedro <pedrohperoni@gmail.com>
2022-01-21 13:43:41 -0300

    json express import fix

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
-app.use(express.json());
+app.use(json());
 app.use(cors());
 
 const tweets = [];
